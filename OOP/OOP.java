4b299870c449fe962dade15339ba37bf22a2aa61
bhtrwhyou <bahterawahyualmunirr@gmail.com>
2025-09-18 19:29:57 +0700

    Update OOP.java

--- OOP/OOP.java
+++ OOP/OOP.java
@@ -5,16 +5,16 @@ public class OOP {
     public static void main(String[] args) {
         
         Buku buku1 = new Buku();
-        buku1.namaBuku = "Epep Booyah";
-        buku1.penulis = "Edo";
-        buku1.penerbit = "PT Garam";
+        buku1.namaBuku = "Cara menjadi Pria Seajati";
+        buku1.penulis = "wahyu";
+        buku1.penerbit = "PT Cahaya mentari";
         buku1.tahunTerbit = 2025;
         
         
         Buku buku2 = new Buku();
-        buku2.namaBuku = "Java Tutorial";
-        buku2.penulis = "Rasyid";   
-        buku2.penerbit = "PT Gula";
+        buku2.namaBuku = "Python Tutorial";
+        buku2.penulis = "Wahyu";   
+        buku2.penerbit = "PT Citra Jaya";
         buku2.tahunTerbit = 2020;
         
         buku1.infoBuku();
@@ -25,3 +25,4 @@ public class OOP {
     }
     
 }
+
